06f2300e3167076389b9df6bcd1402f9c06ef35a
SgtWilko <null@sgtwilko.f9.co.uk>
2018-10-24 22:53:19 +0100

    Changes to improve font loading.

--- clock.py
+++ clock.py
@@ -30,12 +30,24 @@ lastDateFlip = int(round(time.time() * 1000))
 lastSecondFlip = int(round(time.time() * 1000))
 lastScrollTick = int(round(time.time() * 1000))
 
+fonts={}
+
+def loadFont(font):
+    global fonts
+    fonts[font] = graphics.Font()
+    fonts[font].LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/" + font + ".bdf")
+
+loadFont('7x13B')
+loadFont('9x18B')
+loadFont('6x9')
 
 # Create the buffer canvas
 MyOffsetCanvas = MyMatrix.CreateFrameCanvas()
+#oldSecs=0
+#f=0
 while(1):
-    font = graphics.Font()
-    font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/7x13B.bdf")
+    #font = graphics.Font()
+    #font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/7x13B.bdf")
     
     currentDT = datetime.datetime.now()
     
@@ -47,7 +59,13 @@ while(1):
 
     
     Millis = int(round(time.time() * 1000))
-    
+#    Secs = int(round(time.time()))
+#    print(Secs)
+#    if Secs!=oldSecs:
+#        print(f)
+#        oldSecs=Secs
+#        f=0
+#    f+=1
     if Millis-lastSecondFlip>1000:
         lastSecondFlip = int(round(time.time() * 1000))
         tick = not tick
@@ -73,14 +91,16 @@ while(1):
       
     # Load up the font (use absolute paths so script can be invoked from /etc/rc.local correctly)
 
-    graphics.DrawText(MyOffsetCanvas, font, scroller, 28, BLUE, fulldate)
+    graphics.DrawText(MyOffsetCanvas, fonts['7x13B'], scroller, 28, BLUE, fulldate)
 
 
-    font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/9x18B.bdf")
-    graphics.DrawText(MyOffsetCanvas, font, sizeoftime, 14, RED, thetime)
+#    font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/9x18B.bdf")
+    graphics.DrawText(MyOffsetCanvas, fonts['9x18B'], sizeoftime, 14, RED, thetime)
 
-    font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/6x9.bdf")
-    graphics.DrawText(MyOffsetCanvas, font, 50, 14, GREEN, pmam)
+#    font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/6x9.bdf")
+    graphics.DrawText(MyOffsetCanvas, fonts['6x9'], 50, 14, GREEN, pmam)
     
     MyOffsetCanvas = MyMatrix.SwapOnVSync(MyOffsetCanvas)
     MyOffsetCanvas.Clear()
+    #print('here %s' % Millis)
+    time.sleep(0.05)
